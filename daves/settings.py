8c68bf417e6859e1737a01cbf1937bab82807b1a
timo25 <42582369+timo25@users.noreply.github.com>
2021-04-20 15:03:45 +0000

    Update settings.py

--- daves/settings.py
+++ daves/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mydb',
-        'USER': 'carter',
-        'PASSWORD': 'Timo-7436',
-        'HOST': 'localhost',
-        'PORT': '',
+        #'USER': 'carter',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
